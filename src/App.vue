a3d5040be1c6d61e8d4f742506e84db20848f869
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-08-16 15:59:14 +0200

    high

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <navbar/>
   <router-view/>
 </template>
+
+<script>
+import navbar from "./views/NavbarView.vue"
+export default{
+  components:{
+    navbar
+  }
+}
+</script>
 
 <style src="../src/assets/css/style.css"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <h1>this the homepage</h1>
+      <h1 class="mt-5">this the homepage</h1>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
